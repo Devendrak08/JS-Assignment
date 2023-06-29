e36ae7d041d7c35495e6f66bd298475c27d3c92b
Devendrak08 <devendrakkumar08@gmail.com>
2023-06-29 13:33:03 +0530

    task-done

--- LeapYear.js
+++
@@ -1,9 +0,0 @@
-function LeapYear(year) {
-  //comparing year is leap year or not
-  if ((0 == year % 4 && 0 != year % 100) || 0 == year % 400) return true
-  return false
-}
-console.log(LeapYear(2016))
-
-//Output: [true]
-
--- alphabeticalOrder.js
+++
@@ -1,7 +0,0 @@
-function alphabeticalOrder(str) {
-  //Sorting given string to alphabetical order
-  return str.split("").sort().join("")
-}
-console.log(alphabeticalOrder("devendra"))
-
-//output: "addeenrv"
